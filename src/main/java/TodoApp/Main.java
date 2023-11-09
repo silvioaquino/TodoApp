3d71d46c688e1136c66fcd9036e081d6efaca82e
unknown <silvios.aquino@gmail.com>
2023-11-09 10:57:40 -0300

    first commit

---
+++ src/main/java/TodoApp/Main.java
@@ -0,0 +1,13 @@
+package TodoApp;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
