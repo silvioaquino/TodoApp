c54a4c34db5adfecbe6b6c7003c545354bd209d0
unknown <silvios.aquino@gmail.com>
2023-11-09 22:39:11 -0300

    second commit

---
+++ src/main/java/util/connectionFactory.java
@@ -0,0 +1,14 @@
+package util;
+
+
+
+public class connectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
+    
+    
+}
